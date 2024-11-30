ba902a7bbdb72560d255b40f031512a44d36df88
Nika Bostoganashvili <151997372+theBosto@users.noreply.github.com>
2024-11-30 13:24:18 +0400

    structure

---
+++ src/atoms/HeaderElement/HeaderElement.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HeaderElement = () => {
+  return (
+    <div>HeaderElement</div>
+  )
+}
+
+export default HeaderElement
---
+++ src/atoms/SectionElement/SectionElement.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SectionElement = () => {
+  return (
+    <div>SectionElement</div>
+  )
+}
+
+export default SectionElement
